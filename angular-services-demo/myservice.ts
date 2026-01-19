28a2c76e65b1f0e1e54dcc7472f52fbc19b3accc
maryyy111 <123358370+maryyy111@users.noreply.github.com>
2026-01-19 17:29:31 +0800

    Add files via upload

---
+++ angular-services-demo/myservice.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Myservice {
+  showTodayDate() {
+    let ndate = new Date();
+    return ndate;
+  }
+  serviceproperty = 'My Service Component';
+}
